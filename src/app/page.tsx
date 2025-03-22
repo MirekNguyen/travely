d10979274d0604661e125120d7ec03d99fe88549
Mirek Nguyen <mirek.nguyen@o2.cz>
2025-03-22 20:34:02 +0100

    feat: debug to fix error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import PriceExplorer from "./PriceExplorer";
 
 export default async function Home() {
   const prices = await getPrices();
+  console.log(prices);
     return (
     <div className="min-h-screen bg-black text-white">
       <PriceExplorer initialData={prices} />
